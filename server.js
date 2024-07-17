ab1fcf193210cdd2831f95fcf1c2c4487ce88df6
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-07-18 00:35:51 +0530

    User service created

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-import { ServiceBroker } from 'moleculer';
-const broker = new ServiceBroker();
+import UserService from './services/auth.service.js';
 
-// Greeter service
-broker.createService({
-  name: 'greeter',
-  actions: {
-    sayHello(ctx) {
-      return `"Hello ${ctx.params.name}`;
-    },
-  },
-});
+const startApp = async () => {
+  // Start services
+  await UserService.start();
 
-async function startApp() {
-  await broker.start();
-  const res = await broker.call('greeter.sayHello', { name: 'Indrajith' });
-  console.log(res);
-  broker.stop();
-}
+  try {
+    // Simulate user creation
+    const newUser = await UserService.call('user.createUser', {
+      userName: 'Indrajith',
+      email: 'indrajith@test.com',
+    });
+    console.log('New user created: ', newUser);
+    const users = await UserService.call('user.getUsers');
+    console.log('All users: ', users);
+  } catch (error) {
+    console.log('Error', error);
+  } finally {
+    await UserService.stop();
+  }
+};
 
 startApp();
